90e57055b7b957cb4204e31337effc81c5be388a
dsaldana <dajulian@gmail.com>
2013-11-21 18:10:57 -0200

    parameters for pose are added

--- scripts/frame_broadcaster.py
+++ scripts/frame_broadcaster.py
@@ -13,8 +13,19 @@ if __name__ == '__main__':
 
 	target_frame = rospy.get_param("~target_frame")
 
+	# Camera position
+	# Translation
+	x = rospy.get_param("~x",0)
+	y = rospy.get_param("~y",0)
+	z = rospy.get_param("~z",0)
+	# Pose quaternion
+	qm = rospy.get_param("~qm",0)
+	qx = rospy.get_param("~qx",0)
+	qy = rospy.get_param("~qy",0)
+	qz = rospy.get_param("~qz",1)
+
 	while not rospy.is_shutdown():
 
-		br.sendTransform((0.0, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0), rospy.Time.now(), target_frame, "world")
+		br.sendTransform((x,y,z), (qm, qx, qy, qz), rospy.Time.now(), target_frame, "world")
 
 		rate.sleep()
